8a73cfea82f74f36dbf72115c3b70db6d81a29f8
PaulBerra <paul.berra.pro@gmail.com>
2026-07-30 19:09:41 +0200

    feat: integrate full scan pipeline in main

--- cli/main.c
+++ cli/main.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include "utils/hash.h"
 #include "analysis/static/signature.h"
+#include "detection/heuristic.h"
+#include "utils/config.h"
+
+
 
 int main(int argc, char *argv[]) {
     // Vérification du nombre d'arguments
@@ -15,7 +19,9 @@ int main(int argc, char *argv[]) {
     const char *filePath = argv[1];
     printf("Processing file: %s\n", filePath);
 
-    
+    ScanResult result = {0};
+
+
     uint8_t *fileData = NULL;
     size_t fileSize = 0;
 
@@ -24,22 +30,19 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    if (PE_IsValid(fileData, fileSize) != 0) {
+    if (PE_IsValid(fileData, fileSize) != 0) { // et que c'est un exe
         fprintf(stderr, "Invalid PE file: %s\n", filePath);
         free(fileData);
         return EXIT_FAILURE;
     }
     printf("Valid PE file: %s\n", filePath);
 
-    if (PE_ParseSections(fileData, fileSize) != 0) {
+    if (PE_ParseSections(fileData, fileSize, &result) != 0) {
         fprintf(stderr, "Failed to parse PE sections for file: %s\n", filePath);
         free(fileData);
         return EXIT_FAILURE;
     }
     printf("PE sections parsed successfully for file: %s\n", filePath);
-
-
-
 
 
     uint8_t hashOutput[65] = {0}; // 64 characters for SHA256 in hex + null terminator
@@ -55,17 +58,24 @@ int main(int argc, char *argv[]) {
     SignatureEntry *signatures = NULL;
     int signatureCount = 0;
 
-    if (loadCSV("rules\\signatures\\hashes.csv", &signatures, &signatureCount) != 0) {
+    if (loadCSV(CSV_PATH, &signatures, &signatureCount) != 0) {
         fprintf(stderr, "Failed to load signatures\n");
         free(fileData);
         return EXIT_FAILURE;
     }
 
-    const SignatureEntry *match = lookupHash((const char*)hashOutput, signatures, signatureCount);
-    if (match != NULL) {
-        printf("Malware detected: %s\n",match->malwareName);
-    } else {
-        printf("No malware detected for file: %s\n", filePath);
+    result.signatureMatch = lookupHash((const char*)hashOutput, signatures, signatureCount) ? 1 : 0;
+
+    int score = computeScore(&result);
+
+
+    if (score > 70) {
+        printf("\n\n\nVerdict : \n");
+        printf("Malware detected in %s\n", filePath);
+    }
+    else {
+        printf("\n\n\nVerdict : \n");
+        printf("%s look clean.\n", filePath);
     }
 
     free(fileData);
