564ea5e75698fc4be1722a5a1cd18b2a3e515dc4
PaulBerra <paul.berra.pro@gmail.com>
2026-07-30 19:09:40 +0200

    feat: add config header with alert threshold and CSV path

--- utils/config.h
+++ utils/config.h
@@ -1,1 +1,10 @@
 // Fichier de config (JSON/INI)
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ALERT_THRESHOLD 60
+#define ENTROPY_THRESHOLD 7.1
+#define CSV_PATH "rules\\signatures\\hashes.csv"
+
+#endif
