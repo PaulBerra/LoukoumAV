6b3cc60d45f16baf314aea669a9237472019b2be
PaulBerra <paul.berra.pro@gmail.com>
2026-07-30 19:09:40 +0200

    feat: add heuristic scoring engine

--- detection/heuristic.c
+++ detection/heuristic.c
@@ -1,1 +1,29 @@
 // Moteur heuristique + scoring
+#include "heuristic.h"
+
+int computeScore(const ScanResult *result) {
+
+    int score = 0;
+
+    if (result->signatureMatch > 0) {
+        score = score + 100;
+    }
+
+    //if (result->invalidPEHeader) //inutile on a un exit failure si pas ok 
+
+    if (result->suspiciousImports > 1 && result->suspiciousImports <= 3){
+        score = score + 60;
+    }
+    if (result->suspiciousImports > 3 ) {
+        score = score + 100;
+    }
+
+    if (result->suspiciousSections > 2) {
+        score = score + 40;
+    }
+
+    if (result->entropy >= 7.1) {
+        score = score + 30;
+    }
+    return score;
+}
--- detection/heuristic.h
+++ detection/heuristic.h
@@ -1,1 +1,18 @@
 // Moteur heuristique + scoring
+
+#ifndef HEURISTIC_H
+#define HEURISTIC_H
+
+#include <stdint.h>
+
+typedef struct ScanResult {
+    double entropy;
+    int suspiciousImports;
+    int suspiciousSections;
+    int invalidPEHeader;
+    int signatureMatch;
+} ScanResult;
+
+int computeScore(const ScanResult *result);
+
+#endif
