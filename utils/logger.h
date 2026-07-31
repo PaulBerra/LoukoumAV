03de3e0a749686358c83e1c5df2b8a832b36ab95
kraylow <nwo1825.eussou@gmail.com>
2026-07-31 17:02:40 +0200

    feat: ajoute les prototypes et macros de logging

--- utils/logger.h
+++ utils/logger.h
@@ -1,1 +1,33 @@
 // Logging structuré
+
+#ifndef AV_LOGGER_H
+#define AV_LOGGER_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef enum {
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_FATAL
+} LogLevel;
+
+/* Initialize the logger.
+ * @param log_file_path: path to the logging file, NULL for CLI use
+ * @param min_level: minimum logging level displayed
+ */
+int logger_init(const char* log_file_path, LogLevel min_level);
+
+void logger_close(void);
+
+void logger_log(LogLevel level, const char* file, int line, const char* format, ...);
+
+#define LOG_DEBUG(...) logger_log(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...)  logger_log(LOG_LEVEL_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...)  logger_log(LOG_LEVEL_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) logger_log(LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_FATAL(...) logger_log(LOG_LEVEL_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+
+#endif // AV_LOGGER_H
