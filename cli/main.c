ef9bf8ff370627010bc4cce6e7288121c9166e9f
kraylow <nwo1825.eussou@gmail.com>
2026-08-08 22:35:41 +0200

    feat: inclue un système de verbosité

--- cli/main.c
+++ cli/main.c
@@ -9,6 +9,7 @@
 #include "core/scanner.h"
 #include "core/report.h"
 #include "core/service.h"
+#include "utils/logger.h"
 
 int main(int argc, char *argv[]) {
     /*
@@ -20,10 +21,41 @@ int main(int argc, char *argv[]) {
         run              - internal command called by the SCM
     */
 
-    if (argc < 2 || (strcmp(argv[1], "scan") != 0 
-              && strcmp(argv[1], "install") != 0 
-              && strcmp(argv[1], "uninstall") != 0 
-              && strcmp(argv[1], "run") != 0)) 
+    LogLevel min_level = LOG_LEVEL_INFO;
+    int v_count = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-' && argv[i][1] == 'v') {
+            int valid = 1;
+            for (int j = 1; argv[i][j] != '\0'; j++) {
+                if (argv[i][j] != 'v') {
+                    valid = 0;
+                    break;
+                }
+            }
+            if (valid) {
+                v_count += (int)strlen(argv[i]) - 1;
+            }
+        } else if (strcmp(argv[i], "--quiet") == 0 || strcmp(argv[i], "-q") == 0) {
+            min_level = LOG_LEVEL_WARN;
+        }
+    }
+
+    if (v_count == 1) {
+        min_level = LOG_LEVEL_DEBUG;
+    } else if (v_count >= 2) {
+        min_level = LOG_LEVEL_TRACE;
+    }
+
+    if (logger_init(SERVICE_LOGFILE, min_level) != 0) {
+        fprintf(stderr, "Critical Error : Can't initialize logger.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (argc < 2 || (strcmp(argv[1], "scan") != 0
+              && strcmp(argv[1], "install") != 0
+              && strcmp(argv[1], "uninstall") != 0
+              && strcmp(argv[1], "run") != 0))
     {
         fprintf(stderr, "Invalid command.\n");
         fprintf(stderr, "Usage:\n");
@@ -72,5 +104,6 @@ int main(int argc, char *argv[]) {
     }
 
     fprintf(stderr, "Unknown command: %s\n", argv[1]);
+    logger_close();
     return EXIT_FAILURE;
 }
