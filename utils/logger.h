4bd1df3409c0ceed8b62a5c578a3b4716689dd9b
kraylow <nwo1825.eussou@gmail.com>
2026-08-08 21:47:32 +0200

    style: réorganise la structure des niveaux de log

--- utils/logger.h
+++ utils/logger.h
@@ -24,8 +24,8 @@ void logger_close(void);
 
 void logger_log(LogLevel level, const char* file, int line, const char* format, ...);
 
-#define LOG_DEBUG(...) logger_log(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_INFO(...)  logger_log(LOG_LEVEL_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_DEBUG(...) logger_log(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_WARN(...)  logger_log(LOG_LEVEL_WARN,  __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_ERROR(...) logger_log(LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_FATAL(...) logger_log(LOG_LEVEL_FATAL, __FILE__, __LINE__, __VA_ARGS__)
